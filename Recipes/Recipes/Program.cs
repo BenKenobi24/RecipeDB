c6fa011d8860b636cff4fa0ee4f1e77d2c49b003
BenKenobi24 <thesauce24@gmail.com>
2015-04-13 01:04:10 -0700

    made a recipe
    playing with git

---
+++ Recipes/Recipes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipes
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Recipe pizza = new Recipe();
+            pizza.Name = "pizza";
+            pizza.Price = 10;
+
+            Console.WriteLine(pizza);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Recipes/Recipes/Recipe.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipes
+{
+    class Recipe
+    {
+        private String name;
+        public string Name {
+            get { return name; }
+            set { name = value; }
+        }
+        private double price;
+
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public override string ToString()
+        {
+            return name + ":" + price;
+        }
+    }
+}
